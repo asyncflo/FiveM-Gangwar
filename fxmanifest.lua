8c3964ba34a8525b5592d8c7d1a5dd3b9c1e1d6f
asyncflo <122392245+asyncflo@users.noreply.github.com>
2024-12-09 22:42:36 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'asyncflo'
+description 'Gangwar and Garage System with UI'
+
+client_scripts {
+    'gangwar_client.lua',
+    'garage_client.lua'
+}
+
+server_scripts {
+    'gangwar.lua',
+    'garage.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/app.js'
+}
